44f153628006ba624533b4c37e2c79b9b50690a8
Ben McMurtry <benmcmurtry@BenMcMurtry.local>
2016-12-29 13:53:27 +0000

    Initial Commit

---
+++ PR2/main.c
@@ -0,0 +1,15 @@
+//
+//  main.c
+//  PR2
+//
+//  Created by Ben McMurtry on 29/12/2016.
+//  Copyright © 2016 Ben McMurtry. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
